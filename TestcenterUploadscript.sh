c99017aeaaafe961a9981cdd2d32cefce0697758
openBSDheart <35777898+openBSDheart@users.noreply.github.com>
2020-03-27 11:00:07 -0700

    Delete TestcenterUploadscript.sh

--- TestcenterUploadscript.sh
+++
@@ -1,6 +0,0 @@
-#Containeraufruf
-#Aktueller Block (dieser existiert natürlich noch nicht)
-peer channel fetch 0 block -c mychannel
-exit
-#docker cp peer0.org1.example.com:/opt/gopath/src/github.com/hyperledger/fabric/peer/block block
-#../bin/./configtxlator proto_decode --input block --type common.Block > block.decoded
