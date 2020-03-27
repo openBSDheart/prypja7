5e20012dd97e728c238aea28c45f2fa5a6f48ec6
openBSDheart <35777898+openBSDheart@users.noreply.github.com>
2020-03-27 10:59:29 -0700

    Delete TestcenterFetchBlock.sh

--- TestcenterFetchBlock.sh
+++
@@ -1,9 +0,0 @@
-#Containeraufruf
-docker exec -it peer0.org1.example.com bash
-
-#Aktueller Block (dieser existiert natürlich noch nicht)
-peer channel getinfo -c mychannel
-
-peer channel fetch 0 block -c mychannel 
-docker cp peer0.org1.example.com:/opt/gopath/src/github.com/hyperledger/fabric/peer/block block
-./configtxlator proto_decode --input block --type common.Block > block.decoded
